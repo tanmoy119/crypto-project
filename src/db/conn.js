facbd6a7eef6335c8cea41cf73b35443ba109222
unknown <srroy119@gmail.com>
2022-07-06 20:28:30 +0530

    db commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/cryptoDB').then(()=>{
+mongoose.connect('mongodb+srv://TanmoyAtlas1234:TanmoyAtlas1234@cluster0.qebswfo.mongodb.net/criptodb?retryWrites=true&w=majority').then(()=>{
     console.log("connection successfull..");
 }).catch(()=>{
     console.log(err);
